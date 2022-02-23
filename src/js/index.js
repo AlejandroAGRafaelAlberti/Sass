cbcf1033517218d7edf0f3e18f04aeafbbff0fd7
Aletuno <aaceytuno.alumno@iesrafaelalberti.es>
2022-02-23 14:56:36 +0100

    advancementes

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,8 @@
+//carrousel code
 var slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
   showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-  showSlides(slideIndex = n);
 }
 function showSlides(n) {
   var i;
